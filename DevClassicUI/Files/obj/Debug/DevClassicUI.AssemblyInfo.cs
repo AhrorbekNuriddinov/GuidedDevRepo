110e235f29f1a9e146ced517938760c7241a8c58
nuriddinov ahrorbek <nuriddinovahrorbe173@gmail.comk>
2024-05-12 23:18:33 +0500

    RealtyTypeClassic

--- DevClassicUI/Files/obj/Debug/DevClassicUI.AssemblyInfo.cs
+++ DevClassicUI/Files/obj/Debug/DevClassicUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevClassicUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3795bd60ae4d2592b5109ba13e28a7126538735c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf9fb11a90cd1864001fb10ce1e1a772bb3a98fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevClassicUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevClassicUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
